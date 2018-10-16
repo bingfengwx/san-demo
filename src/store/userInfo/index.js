e94abae6b7e554c41b354bf1d2270b7c759ac9f5
vanson <bingfengwx@qq.com>
2018-10-16 19:49:11 +0800

    修改action-name引用的名字为actionNames，避免造成混淆

--- src/store/userInfo/index.js
+++ src/store/userInfo/index.js
@@ -1,11 +1,11 @@
 import { store } from 'san-store';
 import { updateBuilder } from 'san-update/index';
-import * as actions from './action-name';
+import * as actionNames from './action-name';
 
-store.addAction(actions.INIT, function() {
+store.addAction(actionNames.INIT, function() {
     return updateBuilder().set('userInfo.name', 'World');
 });
 
-store.addAction(actions.SET_NAME, function(name) {
+store.addAction(actionNames.SET_NAME, function(name) {
     return updateBuilder().set('userInfo.name', name);
 });
